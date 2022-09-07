2e9cdfebc543348b3dff3c363986b2be2fc921b9
Olegsander78 <onkotov@gmail.com>
2022-09-07 14:27:07 +0500

    Created spawning the characters

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -31,4 +31,45 @@ public class GameManager : MonoBehaviour
             instance = this;
         }
     }
+    private void Start()
+    {
+        CreateCharacters(playerPersistentData, defaultEnemySet);
+    }
+
+    void CreateCharacters(PlayerPersistentData playerData, CharacterSet enemyTeamSet)
+    {
+        playerTeam = new Character[playerData.characters.Length];
+        enemyTeam = new Character[enemyTeamSet.characters.Length];
+
+        int playerSpawnIndex = 0;
+
+        for(int i = 0; i < playerData.characters.Length; i++)
+        {
+            if (!playerData.characters[i].isDead)
+            {
+                Character character = CreateCharacter(playerData.characters[i].characterPrefab, playerTeamSpaws[playerSpawnIndex]);
+                character.curHp = playerData.characters[i].health;
+                playerTeam[i] = character;
+                playerSpawnIndex++;
+            }
+            else
+            {
+                playerTeam[i] = null;
+            }
+        }
+
+        for(int i = 0; i < enemyTeamSet.characters.Length; i++)
+        {
+            Character character = CreateCharacter(enemyTeamSet.characters[i], enemyTeamSpaws[i]);
+            enemyTeam[i] = character;
+        }
+
+        allCharacters.AddRange(playerTeam);
+        allCharacters.AddRange(enemyTeam);
+    }
+    Character CreateCharacter(GameObject characterPrefab, Transform spawnPos)
+    {
+        GameObject obj = Instantiate(characterPrefab, spawnPos.position, spawnPos.rotation);
+        return obj.GetComponent<Character>();
+    }
 }
