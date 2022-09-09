7f036e8b232a35265820e1cc707317113ddd017d
Olegsander78 <onkotov@gmail.com>
2022-09-09 11:18:57 +0500

    Implemented - Enemy Combat manager

--- Assets/Scripts/Combat Actions/RangedCombatAction.cs
+++ Assets/Scripts/Combat Actions/RangedCombatAction.cs
@@ -8,6 +8,8 @@ public class RangedCombatAction : CombatAction
     public GameObject projectilePrefab;
     public override void Cast(Character caster, Character target)
     {
+        if (caster == null)
+            return;
         GameObject proj = Instantiate(projectilePrefab, caster.transform.position + new Vector3(0f, 0.5f, 0f), Quaternion.identity);
         proj.GetComponent<Projectile>().Initialized(target);
     }
